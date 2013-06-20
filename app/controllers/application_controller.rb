ca8859cac30553906605fcc53a5ca0ec09782609
Zen <zen@Zen-Vostro.(none)>
2013-06-20 22:42:27 +0800

    Modify application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :set_cache_buster
 
+  #testing push to server
+  
   protected
 
   def set_cache_buster
